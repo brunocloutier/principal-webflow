90074ef1a5da43c62bf68fb201e0eaa674cd92ef
Bruno Cloutier <bruno@principal.studio>
2025-03-28 08:38:00 -0400

    Update archives.js

--- archives.js
+++ archives.js
@@ -5,54 +5,55 @@ Webflow.push(function () {
 	const lightboxData = document.getElementById('lightbox-data');
 	const listItems = document.querySelectorAll('.archives__list-item');
 	const closeButton = document.getElementById('archives-lightbox-close');
-  
-  listItems.forEach(item => {
-		item.addEventListener('click', (e) => {
-      openLightbox(e.target.getAttribute('data-slug'));
-      e.preventDefault();
-    });
-  });
 	
-  function closeLightbox() {
-  	lightbox.addClass('is-hidden');
-    lightboxData.addClass('is-hidden');
-  }
-
-  function openLightbox(slug) {
-    lightbox.removeClass('is-hidden');
+	listItems.forEach(item => {
+		item.addEventListener('click', (e) => {
+			openLightbox(e.target.getAttribute('data-slug'));
+			e.preventDefault();
+		});
+	});
+	
+	function closeLightbox() {
+		lightbox.addClass('is-hidden');
+		lightboxData.addClass('is-hidden');
+	}
+	
+	function openLightbox(slug) {
+		lightbox.removeClass('is-hidden');
 		lightboxData.html('');
-  	lightboxData.load('/archives-collection/' + slug + ' #data', function() {
-    	lightboxData.removeClass('is-hidden');
-    
-    	$('#archives-lightbox-close').bind('click', closeLightbox);
-    	
-      var mainSlider = new Splide('#main-slider', {
-        type      : 'fade',
-        rewind    : true,
-        pagination: false,
-        arrows    : false,
-      } );
-
-      var thumbnailsSlider = new Splide('#thumbnail-slider', {
-        fixedWidth  : 75,
-        fixedHeight : 50,
-        gap         : 5,
-        rewind      : true,
-        pagination  : false,
-        arrows			: true,
-        cover       : true,
-        isNavigation: true,
-        breakpoints : {
-          600: {
-            fixedWidth : 60,
-            fixedHeight: 44,
-          },
-        },
-      } );
-
-      mainSlider.sync(thumbnailsSlider);
-      mainSlider.mount();
-      thumbnailsSlider.mount();
-    });
-  }
+		lightboxData.load('/archives-collection/' + slug + ' #data', function() {
+			lightboxData.removeClass('is-hidden');
+			
+			$('#archives-lightbox-close').bind('click', closeLightbox);
+			
+			var mainSlider = new Splide('#main-slider', {
+				type      : 'fade',
+				rewind    : true,
+				pagination: false,
+				arrows    : false,
+			} );
+			
+			var thumbnailsSlider = new Splide('#thumbnail-slider', {
+				fixedWidth  : 75,
+				fixedHeight : 50,
+				gap         : 5,
+				rewind      : true,
+				pagination  : false,
+				arrows			: true,
+				cover       : true,
+				isNavigation: true,
+				breakpoints : {
+					600: {
+					fixedWidth : 60,
+					fixedHeight: 44,
+					},
+				},
+			} );
+			
+			mainSlider.sync(thumbnailsSlider);
+			mainSlider.mount();
+			thumbnailsSlider.mount();
+		});
+	}
+	
 });
